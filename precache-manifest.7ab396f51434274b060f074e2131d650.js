aa97afd3d8a24e4ee2fbe652125bdc5863ab8dea
mateuscoury <mateuscoury@gmail.com>
2021-03-22 09:28:05 -0300

    Updates

---
+++ precache-manifest.7ab396f51434274b060f074e2131d650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158c42b84c7fc828b70c11be10bd00d9",
+    "url": "mateuscoury.github.io/app-toot-name/index.html"
+  },
+  {
+    "revision": "671a8a4533820a048df3",
+    "url": "mateuscoury.github.io/app-toot-name/static/css/main.1c1db24b.chunk.css"
+  },
+  {
+    "revision": "ed25f6031b8f38cf4c83",
+    "url": "mateuscoury.github.io/app-toot-name/static/js/2.1a0dc02d.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "mateuscoury.github.io/app-toot-name/static/js/2.1a0dc02d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "671a8a4533820a048df3",
+    "url": "mateuscoury.github.io/app-toot-name/static/js/main.35d918c6.chunk.js"
+  },
+  {
+    "revision": "d80038c0cff23f49aaff",
+    "url": "mateuscoury.github.io/app-toot-name/static/js/runtime-main.0260988f.js"
+  }
+]);
